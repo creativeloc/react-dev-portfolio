17edc8c2c0b4701c2fa74bc0fa9cf0621e86701e
Lawrence Caulker <locaulker@gmail.com>
2025-07-04 19:47:51 -0400

    implemented stars in the ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import { ThemeToggle } from "../components/ThemeToggle"
+import { ThemeToggle } from "@/components/ThemeToggle"
+import { StarBackground } from "@/components/StarBackground"
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
       <ThemeToggle />
+
       {/* Background Effects */}
+      <StarBackground />
 
       {/* Navbar */}
 
