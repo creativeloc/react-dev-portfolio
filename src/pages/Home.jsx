3d4365556c8df23e1cd01d6841e39a8711ebb6f7
creativeloc <creativeloc@yahoo.com>
2025-07-04 08:06:03 -0400

    initial project setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen ">
+      {/* Theme Toggle */}
+
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main content */}
+
+      {/* Footer */}
+    </div>
+  )
+}
+export default Home
