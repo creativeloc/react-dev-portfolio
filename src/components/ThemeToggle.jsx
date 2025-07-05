e84a894bd9af7509c8894b575d312c845e1dbfae
Lawrence Caulker <locaulker@gmail.com>
2025-07-05 18:21:04 -0400

    edit light/dark top icon padding

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -33,7 +33,7 @@ export const ThemeToggle = () => {
       type="button"
       onClick={toggleTheme}
       className={cn(
-        "fixed max-sm:hidden to0-5 right-5",
+        "fixed max-sm:hidden top-4 right-5",
         "z-50 p-2 rounded-full transition-colors duration-300",
         "focus:outline-hidden"
       )}
